609a92dc034c8fe8547761fc055dbaf429451832
Dein Name <deine@email.at>
2026-06-09 15:37:39 +0200

    Neue Datei Rechner & Addition

---
+++ Rechner.java
@@ -0,0 +1,8 @@
+public class Rechner {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int summe = a + b;
+        System.out.println("Ergebnis: " + summe);
+    }
+}
