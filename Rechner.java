2f4ad9abbc4a06d3585a70cfbe40fb5783236900
BichlerP <bichlerphilipp3@gmail.com>
2026-06-16 14:38:07 +0200

    Update Rechner.java

--- Rechner.java
+++ Rechner.java
@@ -1,18 +1,1 @@
-import java.util.Scanner;
 
-public class Rechner {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Erste Zahl eingeben:");
-        int a = scanner.nextInt();
-        
-        System.out.println("Zweite Zahl eingeben:");
-        int b = scanner.nextInt();
-        
-        int summe = a + b;
-        System.out.println("Ergebnis: " + summe);
-        
-        scanner.close();
-    }
-}
