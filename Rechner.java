f9ee108639c2c6f6f70a0b09a195bec0f4d46629
Dein Name <deine@email.at>
2026-06-16 14:39:11 +0200

    Rechner zurück auf ursprüngliche Version

--- Rechner.java
+++ Rechner.java
@@ -1,18 +1,9 @@
-import java.util.Scanner;
-
 public class Rechner {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Erste Zahl eingeben:");
-        int a = scanner.nextInt();
-        
-        System.out.println("Zweite Zahl eingeben:");
-        int b = scanner.nextInt();
-        
+        int a = 5;
+        int b = 7;
         int summe = a + b;
         System.out.println("Ergebnis: " + summe);
-        
-        scanner.close();
     }
 }
+
