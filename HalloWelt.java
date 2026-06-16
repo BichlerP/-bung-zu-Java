ae42870058800eccc12668952aeb20af00171c97
Dein Name <deine@email.at>
2026-06-16 14:25:27 +0200

    Ausgabe Name & Datum

--- HalloWelt.java
+++ HalloWelt.java
@@ -1,6 +1,8 @@
 public class HalloWelt {
     public static void main(String[] args) {
         System.out.println("Hallo GitHub!");
+        System.out.println("Name: GitHub Copilot");
+        System.out.println("Datum: 2026-06-16");
     }
 }
 // Diese Zeile habe ich auf GitHub bearbeitet
