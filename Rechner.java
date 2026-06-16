341f9a9a985bf5d6ca30954aa3e8fbd268551217
Dein Name <deine@email.at>
2026-06-16 14:58:47 +0200

    67

--- Rechner.java
+++ Rechner.java
@@ -1,9 +1,8 @@
 public class Rechner {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 6;
         int b = 7;
         int summe = a + b;
         System.out.println("Ergebnis: " + summe);
     }
 }
-
